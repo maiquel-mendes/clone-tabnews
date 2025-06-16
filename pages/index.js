d7c1b7f7ea563fa44beb89042694289165649d14
Maiquel Anderson C. Mendes <62763726+maiquel-mendes@users.noreply.github.com>
2025-06-16 19:44:17 +0000

    alterando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Alterando a mensagem</h1>
+            <h1>Alterando a mensagem agora de forma automatica</h1>
         </div>
     );
 }
