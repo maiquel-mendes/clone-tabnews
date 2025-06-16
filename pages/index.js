49e85c55b3541799c0de69745ab9e7d8baf5a92e
Maiquel Anderson C. Mendes <62763726+maiquel-mendes@users.noreply.github.com>
2025-06-16 21:38:44 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div>
-            <h1>Alterando a mensagem agora de forma automatica</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Alterando a mensagem agora de forma automatica</h1>
+    </div>
+  );
 }
 
 export default Home;
