07b51a32019a99d033d885ad5383aaa4ddb05b54
Maiquel Anderson C. Mendes <62763726+maiquel-mendes@users.noreply.github.com>
2025-06-17 23:16:38 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 const database = require("infra/database.js");
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "a resposta foi ok média" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query(`SHOW server_version;`);
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnResult = await database.query(`SHOW max_connections;`);
+  const dbMaxConn = dbMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dbOpenedConns = dbOpenedConnsResult.rows[0].count;
+
+  console.log(dbVersion, dbMaxConn, dbOpenedConns);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConn),
+        opened_connections: dbOpenedConns,
+      },
+    },
+  });
 }
 
 export default status;
