c7da0394b8418df9d0f1906de74c7c3b12960a6e
Maiquel Anderson C. Mendes <62763726+maiquel-mendes@users.noreply.github.com>
2025-06-17 18:35:49 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+const database = require("../../../../infra/database.js");
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ message: "a resposta foi ok média" });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,9 @@
+const database = require("../../../../../infra/database.js");
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(
     "https://potential-space-fiesta-75qgrvgwgvvcrxr7-3000.app.github.dev/api/v1/status",
   );
+  console.log(response.status);
   expect(response.status).toBe(200);
 });
